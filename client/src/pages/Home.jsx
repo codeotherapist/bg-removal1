39b9eff6604e1c710cc763041af1be760f4f2ba8
codeotherapist <codeo.therapist995@gmail.com>
2025-07-03 14:02:51 +0530

    testimonials conflict

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import BgSlider from '../components/BgSlider'
-import Testimonials from '../components/testimonials'
+import Testimonials from '../components/Testimonials'
 import Upload from '../components/Upload'
 const Home = () => {
   return (
